c41ad26d42d19b98d9e4745ebf31b0eb33db8549
DavidLasserre <david.lasserre.2@ens.etsmtl.ca>
2024-09-30 18:57:19 -0400

    update sql

--- script.sql
+++ script.sql
@@ -1,5 +1,16 @@
+CREATE TABLE Abonnement (
+  code CHAR PRIMARY KEY,
+  forfait VARCHAR(255),
+  cout FLOAT,
+  reservation_max_copies NUMBER(38),
+  duree_max_jours NUMBER(38) NOT NULL
+);
+
 CREATE TABLE Client (
   id_client NUMBER(38) PRIMARY KEY,
+  nom VARCHAR(255) NOT NULL,
+  prenom VARCHAR(255) NOT NULL,
+  date_de_naissance DATE NOT NULL,
   courriel VARCHAR(255) NOT NULL UNIQUE,
   telephone VARCHAR(255) NOT NULL,
   mot_de_passe VARCHAR(255) NOT NULL,
@@ -10,22 +21,32 @@ CREATE TABLE Client (
   carte VARCHAR(15) CHECK (carte IN ('VISA', 'MasterCard', 'Amex')) NOT NULL,
   numero VARCHAR(255) NOT NULL,
   cvv VARCHAR(255) NOT NULL,
-  dateExpiration DATE NOT NULL
+  date_expiration DATE NOT NULL,
+  code_abonnement CHAR REFERENCES Abonnement(code),
+    CONSTRAINT chk_mot_de_passe CHECK (
+    LENGTH(mot_de_passe) >= 5 AND
+    REGEXP_LIKE(mot_de_passe, '.*[A-Za-z].*') AND
+    REGEXP_LIKE(mot_de_passe, '.*[0-9].*')
+  )
 );
 
 CREATE TABLE Employee (
   id_employee NUMBER(38) PRIMARY KEY,
+  nom VARCHAR(255),
+  prenom VARCHAR(255),
+  date_de_naissance DATE,
   courriel VARCHAR(255) UNIQUE NOT NULL,
   telephone VARCHAR(255) NOT NULL,
   mot_de_passe VARCHAR(255) NOT NULL,
   adresse VARCHAR(255) NOT NULL,
   ville VARCHAR(255) NOT NULL,
   province VARCHAR(255) NOT NULL,
-  code_postal VARCHAR(255) NOT NULL
+  code_postal VARCHAR(255) NOT NULL,
+  matricule VARCHAR(7) NOT NULL UNIQUE
 );
 
-CREATE TABLE Réalisateur (
-  id_realisateur NUMBER(38) PRIMARY KEY,
+CREATE TABLE Personne (
+  id_personne NUMBER(38) PRIMARY KEY,
   nom VARCHAR(255),
   prenom VARCHAR(255),
   date_de_naissance DATE,
@@ -42,15 +63,7 @@ CREATE TABLE Film (
   duree_en_minutes NUMBER(38),
   resume_scenario VARCHAR(255) NOT NULL,
   affiche VARCHAR(255),
-  realisateur_id NUMBER(38) NOT NULL REFERENCES Réalisateur(id_realisateur)
-);
-
-CREATE TABLE Abonnement (
-  code CHAR PRIMARY KEY,
-  forfait VARCHAR(255),
-  cout FLOAT,
-  reservation_max_copies NUMBER(38),
-  duree_max_jours NUMBER(38) NOT NULL
+  realisateur_id NUMBER(38) NOT NULL REFERENCES Personne(id_personne)
 );
 
 CREATE TABLE Scenariste (
@@ -75,20 +88,10 @@ CREATE TABLE FilmGenre (
   CONSTRAINT pk_FilmGenre PRIMARY KEY (film_id, genre_id)
 );
 
-CREATE TABLE Acteur (
-  id_acteur NUMBER(38) PRIMARY KEY,
-  nom VARCHAR(255),
-  prenom VARCHAR(255),
-  date_de_naissance DATE,
-  lieu_de_naissance VARCHAR(255),
-  biographie VARCHAR(255),
-  photo VARCHAR(255)
-);
-
 CREATE TABLE Role (
   id_role NUMBER(38) PRIMARY KEY,
   personnage VARCHAR(255),
-  acteur_id NUMBER(38) NOT NULL REFERENCES Acteur(id_acteur),
+  acteur_id NUMBER(38) NOT NULL REFERENCES Personne(id_personne),
   film_id NUMBER(38) NOT NULL REFERENCES Film(id_film)
 );
 
@@ -122,3 +125,68 @@ CREATE TABLE LienVersLaBandeAnnonce (
   lien VARCHAR(255),
   film_id NUMBER(38) NOT NULL REFERENCES Film(id_film)
 );
+
+CREATE OR REPLACE TRIGGER check_max_reservations
+BEFORE INSERT ON Reservation
+FOR EACH ROW
+DECLARE
+    v_reservation_max_copies NUMBER;
+    v_current_rentals NUMBER;
+BEGIN
+    -- Get the max number of reservations
+    SELECT a.reservation_max_copies
+    INTO v_reservation_max_copies
+    FROM Client c
+    JOIN Abonnement a ON c.code_abonnement = a.code
+    WHERE c.id_client = :NEW.client_id;
+
+    -- Count the current active reservations
+    SELECT COUNT(*)
+    INTO v_current_rentals
+    FROM Reservation r
+    JOIN CopieFilm cf ON r.copieFilm_numero_de_code = cf.numero_de_code
+    WHERE r.client_id = :NEW.client_id
+    AND cf.est_retourner = 0;
+
+    -- Check if the client exceeds the max reservations allowed
+    IF v_current_rentals >= v_reservation_max_copies THEN
+        RAISE_APPLICATION_ERROR(-20001, 'The client has reached the maximum number of allowed rentals.');
+    END IF;
+END;
+
+CREATE OR REPLACE TRIGGER check_movie_availability
+BEFORE INSERT ON Reservation
+FOR EACH ROW
+DECLARE
+    v_est_retourner NUMBER(1);
+BEGIN
+    -- Check if the movie copy is available for rental
+    SELECT est_retourner
+    INTO v_est_retourner
+    FROM CopieFilm
+    WHERE numero_de_code = :NEW.copieFilm_numero_de_code;
+
+    -- Raise an error if the movie copy is not available (i.e., not returned)
+    IF v_est_retourner = 0 THEN
+        RAISE_APPLICATION_ERROR(-20002, 'The selected movie copy is not available for rental.');
+    END IF;
+END;
+
+CREATE OR REPLACE TRIGGER trg_check_credit_card_expiration
+BEFORE INSERT OR UPDATE ON Client
+FOR EACH ROW
+BEGIN
+  IF :NEW.date_expiration < TRUNC(SYSDATE) THEN
+    RAISE_APPLICATION_ERROR(-20001, 'Expiration date must be today or in the future.');
+  END IF;
+END;
+
+CREATE OR REPLACE TRIGGER trg_check_age
+BEFORE INSERT OR UPDATE ON Client
+FOR EACH ROW
+BEGIN
+  -- Calculate the age in years
+  IF TRUNC(MONTHS_BETWEEN(SYSDATE, :NEW.date_de_naissance) / 12) < 18 THEN
+    RAISE_APPLICATION_ERROR(-20002, 'Client must be at least 18 years old.');
+  END IF;
+END;
