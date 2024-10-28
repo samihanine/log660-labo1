aa66f8c7c5c7099d8fcb4c4e3bb54bddd93b6be3
Serge BROU <brouserge1er@gmail.com>
2024-10-28 18:47:47 -0400

    add application.properties file

--- src/main/java/ca/etsmtl/log660/labo2/Labo2Application.java
+++ src/main/java/ca/etsmtl/log660/labo2/Labo2Application.java
@@ -2,7 +2,6 @@ package ca.etsmtl.log660.labo2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication
 public class Labo2Application {
@@ -10,5 +9,4 @@ public class Labo2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Labo2Application.class, args);
 	}
-
 }
